13f9522187a1d215b62aa65c38e9f3749d7f0bff
dzivor <dzivordaniel144@gmail.com>
2026-04-21 14:27:54 +0000

    feat: implement ESB statement generation and account lookup functionality

--- umb-portal/src/Routes/Routes.tsx
+++ umb-portal/src/Routes/Routes.tsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router";
 import Welcome from "../Components/Welcome";
 import Login from "../Components/Login";
 import Statement from "../Components/Statement";
+import ESBStatement from "../Components/ESB-Statement";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/welcome" element={<Welcome />} />
-      <Route path="/statement" element={<Statement />} />
+      <Route path="/Statement" element={<Statement />} />
+      <Route path="/ESB-Statement" element={<ESBStatement />} />
     </Routes>
   );
 };
